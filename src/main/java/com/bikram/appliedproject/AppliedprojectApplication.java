95b5f4aa3d50d4be34537a3cc1b499460e971d2a
Bikram <sbikrm@gmail.com>
2021-08-04 15:32:24 +0800

    Initial Commit

---
+++ src/main/java/com/bikram/appliedproject/AppliedprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bikram.appliedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliedprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppliedprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bikram/appliedproject/AppliedprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikram.appliedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliedprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
